0b81465b59d751c72c39941c91446d7e771fd686
SGiuri <s.giuri@issm.it>
2020-09-21 23:59:00 +0200

    Still some problems

--- anagram.py
+++ anagram.py
@@ -1,2 +1,19 @@
 def find_anagrams(word, candidates):
-    pass
+    selected = []
+    for candidate in candidates:
+        if candidate == word:
+            break
+
+    for candidate in candidates:
+        if ordered_letters(candidate) == ordered_letters(word):
+            selected.append(candidate)
+
+    return selected
+
+
+def ordered_letters(word):
+    new_word = ""
+    for letter in set(word):
+        new_word += letter * word.count(letter)
+
+    return new_word.lower()
